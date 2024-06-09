ffeb613e8fb4480566e1610cb94b4eaead3c3efa
Sungup Hwang <108528528+cocorini@users.noreply.github.com>
2024-06-09 23:16:49 +0900

    [Gold IV] Title: 플로이드, Time: 344 ms, Memory: 43428 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -5,15 +5,6 @@ public class Main {
 	static int N, M;
 	static final int MAX = 100000000;
 	static int[][] visited;
-	static PriorityQueue<Edge> pq = new PriorityQueue<>((e1, e2) -> Integer.compare(e1.w, e2.w));
-	static class Edge {
-		int s, e, w;
-		public Edge(int s, int e, int w) {
-			this.s = s;
-			this.e = e;
-			this.w = w;
-		}
-	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,25 +22,15 @@ public class Main {
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			pq.offer(new Edge(a, b, c));
+			if(visited[a][b] > c) visited[a][b] = c;
 		}
-		while(!pq.isEmpty()) {
-			Edge E = pq.poll();
-			int s = E.s, e = E.e, w = E.w;
-			if(visited[s][e] >= w) {
-				visited[s][e] = w;
-				for (int node = 1; node <= N; node++) {
-					if(visited[s][node] > visited[s][e]+visited[e][node]) {
-						visited[s][node] = visited[s][e]+visited[e][node];
-						pq.offer(new Edge(s, node, visited[s][node]));
-					}
-					if(visited[node][e] > visited[node][s]+visited[s][e]) {
-						visited[node][e] = visited[node][s]+visited[s][e];
-						pq.offer(new Edge(node, e, visited[node][e]));
-					}
-				}
-			}
-		}
+		
+		for (int i = 1; i <= N; i++)
+			for (int j = 1; j <= N; j++)
+				for (int k = 1; k <= N; k++)
+					visited[j][k] = Math.min(visited[j][i]+visited[i][k], visited[j][k]);
+		
+		
 		StringBuilder sb = new StringBuilder();
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= N; j++) {
